dd921fd26ec0240d0c89e3bb86ac1230ba89a240
BoruNot <tiktokborush@gmail.com>
2024-07-01 12:56:33 -0300

    Better Randomizations and added block break

--- src/main/java/keystrokesmod/module/impl/ghost/LeftClicker.java
+++ src/main/java/keystrokesmod/module/impl/ghost/LeftClicker.java
@@ -4,8 +4,10 @@ import keystrokesmod.module.Module;
 import keystrokesmod.module.setting.impl.ButtonSetting;
 import keystrokesmod.module.setting.impl.SliderSetting;
 import keystrokesmod.utility.Utils;
+import net.minecraft.block.Block;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
@@ -18,26 +20,27 @@ public class LeftClicker extends Module {
     public SliderSetting minCPS;
     public SliderSetting maxCPS;
     public SliderSetting jitter;
+    public SliderSetting delay;
     public ButtonSetting weaponOnly;
     public ButtonSetting disableOnInventory;
+    public ButtonSetting breakBlocks;
     private Random rand = new Random();
     private long nextClickTime;
     private long lastClickTime;
     private boolean allow;
+    private boolean alternate;
 
     public LeftClicker() {
         super("Left Clicker", category.ghost, 0);
         this.registerSetting(minCPS = new SliderSetting("Min CPS", 9.0, 1.0, 20.0, 0.5));
         this.registerSetting(maxCPS = new SliderSetting("Max CPS", 12.0, 1.0, 20.0, 0.5));
         this.registerSetting(jitter = new SliderSetting("Jitter", 0.0, 0.0, 3.0, 0.1));
+        this.registerSetting(delay = new SliderSetting("Delay", 1.0, 1.0, 10.0, 1.0));
         this.registerSetting(weaponOnly = new ButtonSetting("Weapon only", false));
         this.registerSetting(disableOnInventory = new ButtonSetting("Disable On Inventory", true));
+        this.registerSetting(breakBlocks = new ButtonSetting("Break Blocks", true));
         this.nextClickTime = System.currentTimeMillis() + getRandomDelay();
-    }
-
-    public String getInfo() {
-        return String.valueOf((int) minCPS.getInput()) + "cps" + " - " + String.valueOf((int) maxCPS.getInput()) + "cps";
-
+        this.alternate = false;
     }
 
     public void guiUpdate() {
@@ -50,6 +53,10 @@ public class LeftClicker extends Module {
         long delay = max > min ? ThreadLocalRandom.current().nextLong(min, max) : min;
         delay += ThreadLocalRandom.current().nextLong(-min / 3, min / 3);
         return delay;
+    }
+
+    public String getInfo() {
+        return String.valueOf((int) delay.getInput());
     }
 
     @SubscribeEvent
@@ -84,19 +91,33 @@ public class LeftClicker extends Module {
             }
 
             if (allow && System.currentTimeMillis() > lastClickTime) {
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
-                KeyBinding.onTick(mc.gameSettings.keyBindAttack.getKeyCode());
-                lastClickTime = System.currentTimeMillis();
+                boolean shouldClick = true;
 
-                if (ThreadLocalRandom.current().nextInt(10) < 2) {
-                    nextClickTime += ThreadLocalRandom.current().nextInt(20, 50);
+                if (breakBlocks.isToggled() && isLookingAtBreakableBlock()) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
+                    KeyBinding.onTick(mc.gameSettings.keyBindAttack.getKeyCode());
+                    lastClickTime = System.currentTimeMillis();
+                    nextClickTime = System.currentTimeMillis() + getRandomDelay();
+                    allow = false;
+                } else if (!breakBlocks.isToggled() || !isLookingAtBreakableBlock()) {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
+                    KeyBinding.onTick(mc.gameSettings.keyBindAttack.getKeyCode());
+                    lastClickTime = System.currentTimeMillis();
+
+                    long randomDelay = getRandomDelay();
+                    nextClickTime = System.currentTimeMillis() + (alternate ? randomDelay / 2 : randomDelay);
+                    alternate = !alternate;
+
+                    if (ThreadLocalRandom.current().nextInt(10) < 2) {
+                        nextClickTime += ThreadLocalRandom.current().nextInt(20, 50);
+                    }
+
+                    allow = false;
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), false);
                 }
-
-                nextClickTime = System.currentTimeMillis() + getRandomDelay();
-                allow = false;
-
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), false);
             }
+        } else {
+            KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), false);
         }
     }
 
@@ -113,5 +134,19 @@ public class LeftClicker extends Module {
         } else {
             player.rotationPitch -= rand.nextFloat() * jitterAmount * 0.45D;
         }
+    }
+
+    private boolean isLookingAtBreakableBlock() {
+        MovingObjectPosition mouseOver = mc.objectMouseOver;
+
+        if (mouseOver != null && mouseOver.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
+            Block block = mc.theWorld.getBlockState(mouseOver.getBlockPos()).getBlock();
+
+            if (block.getBlockHardness(mc.theWorld, mouseOver.getBlockPos()) >= 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
